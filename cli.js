25d7fb8465c3765bb8358d879a9fed1b4c5ac84b
Serguey Martinez <sarellano85@gmail.com>
2020-05-03 20:40:57 -0400

    chore: add version opt to cli
    Closes #4

--- cli.js
+++ cli.js
@@ -4,6 +4,11 @@ const client = require('./client')
 const topic = argv._[0]
 const suffix = argv.s
 
+if (argv.v || argv.version) {
+  console.log(require('./package.json').version)
+  process.exit(0)
+}
+
 if (topic) client(topic, suffix)
 else {
   console.log('Provide a topic please')
